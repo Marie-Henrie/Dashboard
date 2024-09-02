badc94457d85fe7375c989f890a130b0db9933e3
Marie-Henrie <marie.sonninen@gmail.com>
2024-09-03 00:21:09 +0300

    paivtys 8 kuvaan

--- memorygame/index.js
+++ memorygame/index.js
@@ -1,8 +1,7 @@
 const cardImages = [
     'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 
-    'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg',
-    'image9.jpg', 'image10.jpg', 'image11.jpg', 'image12.jpg'
-]; // These should match the image filenames in your pics/ folder
+    'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg'
+   ]; // These should match the image filenames in your pics/ folder
 
 let cards, firstCard, secondCard;
 let lockBoard = false;
